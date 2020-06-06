dd81dc3ef06ff98cf02c7be475a4759dcb047dd2
Marcos Fernandes <marcos@development.local>
2020-06-06 19:16:34 -0300

    Add styled-components configuration.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { Router } from './navigation/router'
 import store from './state/store'
+import GlobalStyles from './ui/styles/global'
 
 const App = () => (
   <Provider store={store}>
+    <GlobalStyles />
     <Router />
   </Provider>
 )
